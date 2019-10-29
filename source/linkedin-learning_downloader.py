69ca8c1d9df6be4d62c329723c6442e696a37449
druvaciam <druvaciam@gmail.com>
2019-10-29 20:38:16 +0300

    futher refactoring

--- source/linkedin-learning_downloader.py
+++ source/linkedin-learning_downloader.py
@@ -19,10 +19,7 @@ import codecs
 import argparse
 
 
-# global fields
 timeout_sec = 5
-autoplay_postfix = "?autoplay=true"
-login_link = 'https://www.linkedin.com/uas/login?fromSignIn=true&trk=learning&_l=en_US&uno_session_redirect=https%3A%2F%2Fwww.linkedin.com%2Flearning%2F%3Ftrk%3Ddefault_guest_learning&session_redirect=%2Flearning%2FloginRedirect.html&is_enterprise_authed='
 
 
 def save_html(html, file_path):
@@ -92,21 +89,11 @@ class Arguments:
         return []
 
 
-if __name__ == '__main__':
-    cmdline_args = Arguments()
-
-    user_password = cmdline_args.get_user_password()
-    base_dir = cmdline_args.get_content_derectory()
-    user_email = cmdline_args.get_user_email()
-    courses = cmdline_args.get_courses() # ['https://www.linkedin.com/learning/c-plus-plus-design-patterns-creational']
-    print('courses to download:', courses)
-
-
-    driver = webdriver.Firefox(executable_path = cmdline_args.get_driver_path())
+def get_logged_in_driver(driver_path, user_email, user_password, login_link):
+    driver = webdriver.Firefox(executable_path = driver_path)
     driver.set_page_load_timeout(50)
     driver.maximize_window()
 
-    #driver.get("https://www.linkedin.com/learning/login?redirect=https%3A%2F%2Fwww.linkedin.com%2Flearning%2F%3Ftrk%3Ddefault_guest_learning&trk=sign_in")
     driver.get(login_link)
     wait_for_js(driver)
 
@@ -123,70 +110,112 @@ if __name__ == '__main__':
     input_password.send_keys(Keys.RETURN)
     wait_for_js(driver)
     time.sleep(timeout_sec)
+    return driver
 
-    for course_url in courses:
-        try:
-            driver.get(course_url)
-            wait_for_js(driver)
-            time.sleep(timeout_sec)
 
-            save_dir = f"{base_dir}/{course_url.split('/')[-1]}"
-            exersice_dir = f"{save_dir}/Exercice Files"
-            if not os.path.exists(exersice_dir):
-                os.makedirs(exersice_dir)
+class Downloader():
+    def __init__(self, web_driver):
+        self.autoplay_postfix = "?autoplay=true"
+        self.driver = web_driver
+        self.courses = []
+        self.directory_to_store = ''
 
-            tabs = driver.find_elements_by_tag_name('artdeco-tab')
-            for tab in tabs:
-                if 'Contents' in tab.get_attribute('innerHTML'):
-                    tab.click()
-                    wait_for_js(driver)
+    def download(self):
+        driver = self.driver
+        exersise_tab = 'Exercise Files'
+        content_tab = 'Contents'
+        for course_url in self.courses:
+            try:
+                driver.get(course_url)
+                wait_for_js(driver)
+                time.sleep(timeout_sec)
+
+                save_dir = f"{self.directory_to_store}/{course_url.split('/')[-1]}"
+                if not os.path.exists(save_dir):
+                    os.makedirs(save_dir)
+
+                tabs = driver.find_elements_by_tag_name('artdeco-tab')
+                for tab in tabs:
+                    if content_tab in tab.get_attribute('innerHTML'):
+                        tab.click()
+                        wait_for_js(driver)
+                        save_html(driver.page_source, f"{save_dir}/info.html")
+                    elif exersise_tab in tab.get_attribute('innerHTML'):
+                        tab.click()
+                        wait_for_js(driver)
+                        break
+
+                elemets_a = driver.find_elements_by_tag_name('a')
+                vid_refs = []
+                exercise_refs = []
+                for a in elemets_a:
+                    href = a.get_attribute('href')
+                    if course_url in href and self.autoplay_postfix in href:
+                        vid_refs.append(href)
+                    elif '/exercises/' in href:
+                        exercise_refs.append(href)
+
+                if exercise_refs:
+                    exersice_dir = f"{save_dir}/{exersise_tab}"
+                    if not os.path.exists(exersice_dir):
+                        os.makedirs(exersice_dir)
+
+                    for exercise_url in exercise_refs:
+                        try:
+                            #print('exercise url:', exercise_url)
+                            exercise_name = file_name_from_url(exercise_url)
+                            save_path = f"{exersice_dir}/{exercise_name}"
+                            download_file(exercise_url, save_path)
+                        except KeyboardInterrupt:
+                            raise
+                        except:
+                            print(f"\nException during processing {href}:", sys.exc_info()[0])
+                else:
+                    print(f"Warning! {file_name_from_url(course_url)} does't contains {exersise_tab}")
+
+                if vid_refs:
+                    for href in vid_refs:
+                        try:
+                            driver.get(href)
+                            time.sleep(timeout_sec)
+                            vid_elem = driver.find_element_by_tag_name('video')
+                            vid_url = vid_elem.get_attribute('src')
+                            #print('video url:', vid_url)
+                            vid_name = file_name_from_url(vid_url)
+                            save_path = f"{save_dir}/{vid_name}"
+                            download_file(vid_url, save_path)
+                        except KeyboardInterrupt:
+                            raise
+                        except:
+                            print(f"\nException during processing {href}:", sys.exc_info()[0])
+
                     save_html(driver.page_source, f"{save_dir}/info.html")
-                elif 'Exercise Files' in tab.get_attribute('innerHTML'):
-                    tab.click()
-                    wait_for_js(driver)
-                    break
+                else:
+                    print(f"Warning! {file_name_from_url(course_url)} does't contains {content_tab}")
+            except KeyboardInterrupt:
+                raise
+            except:
+                print(f"\nException during processing {course_url}:", sys.exc_info()[0])
 
-            elemets_a = driver.find_elements_by_tag_name('a')
-            vid_refs = []
-            exercise_refs = []
-            for a in elemets_a:
-                href = a.get_attribute('href')
-                if course_url in href and autoplay_postfix in href:
-                    vid_refs.append(href)
-                elif '/exercises/' in href:
-                    exercise_refs.append(href)
 
-            for exercise_url in exercise_refs:
-                try:
-                    #print('exercise url:', exercise_url)
-                    exercise_name = file_name_from_url(exercise_url)
-                    save_path = f"{exersice_dir}/{exercise_name}"
-                    download_file(exercise_url, save_path)
-                except KeyboardInterrupt:
-                    raise
-                except:
-                    print(f"\nException during processing {href}:", sys.exc_info()[0])
+if __name__ == '__main__':
+    cmdline_args = Arguments()
 
-            for href in vid_refs:
-                try:
-                    driver.get(href)
-                    time.sleep(timeout_sec)
-                    vid_elem = driver.find_element_by_tag_name('video')
-                    vid_url = vid_elem.get_attribute('src')
-                    #print('video url:', vid_url)
-                    vid_name = file_name_from_url(vid_url)
-                    save_path = f"{save_dir}/{vid_name}"
-                    download_file(vid_url, save_path)
-                except KeyboardInterrupt:
-                    raise
-                except:
-                    print(f"\nException during processing {href}:", sys.exc_info()[0])
+    user_email = cmdline_args.get_user_email()
+    user_password = cmdline_args.get_user_password()
+    base_dir = cmdline_args.get_content_derectory()
+    courses = cmdline_args.get_courses() # ['https://www.linkedin.com/learning/c-plus-plus-design-patterns-creational']
+    print('courses to download:', courses)
 
-            save_html(driver.page_source, f"{save_dir}/info.html")
-        except KeyboardInterrupt:
-            raise
-        except:
-            print(f"\nException during processing {course_url}:", sys.exc_info()[0])
+    # the next link doesn't work with driver
+    # driver.get("https://www.linkedin.com/learning/login?redirect=https%3A%2F%2Fwww.linkedin.com%2Flearning%2F%3Ftrk%3Ddefault_guest_learning&trk=sign_in")
+    login_link = 'https://www.linkedin.com/uas/login?fromSignIn=true&trk=learning&_l=en_US&uno_session_redirect=https%3A%2F%2Fwww.linkedin.com%2Flearning%2F%3Ftrk%3Ddefault_guest_learning&session_redirect=%2Flearning%2FloginRedirect.html&is_enterprise_authed='
+    driver = get_logged_in_driver(cmdline_args.get_driver_path(), user_email, user_password, login_link)
 
-    print('finish')
+    downloader = Downloader(driver)
+    downloader.courses = courses
+    downloader.directory_to_store = base_dir
+    downloader.download()
+
     driver.quit()
+    print('finish')
